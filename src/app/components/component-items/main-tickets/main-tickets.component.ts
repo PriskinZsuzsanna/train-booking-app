31669ecf849945989b8a4dd6a8ff40864ef8e30d
Priskin Zsuzsanna <priskin.zsuzsanna@gmail.com>
2024-06-24 22:03:13 +0200

    tickets-container

---
+++ src/app/components/component-items/main-tickets/main-tickets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-tickets',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-tickets.component.html',
+  styleUrl: './main-tickets.component.scss'
+})
+export class TrainDepartureMainTicketsComponent {
+
+}
--- src/app/components/pages/home/home.page.component.ts
+++ src/app/components/pages/home/home.page.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { TrainBookingHomeDepartureComponent } from '../../component-items/main-departure/home-departure.component';
+import { TrainDepartureMainTicketsComponent } from '../../component-items/main-tickets/main-tickets.component';
 
 @Component ({
   selector: 'home-page',
   standalone: true,
   templateUrl: './home.page.component.html',
   styleUrl: './home.page.component.scss',
-  imports: [TrainBookingHomeDepartureComponent]
+  imports: [
+    TrainBookingHomeDepartureComponent,
+    TrainDepartureMainTicketsComponent
+  ]
 })
 
 export class TrainBookingHomePageComponent {
